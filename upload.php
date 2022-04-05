237bf93d8760a4d0824036130ea00195d6270760
shoaib <59531766+shoaib-zaheer@users.noreply.github.com>
2022-04-06 00:48:23 +0200

    php file update

--- upload.php
+++ upload.php
@@ -5,4 +5,14 @@ $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["file"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image. ";
+    $uploadOk = 0;
+  }
+}
 ?>
